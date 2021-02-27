1cdd84b796072cdfa4cc4868f3dfd52c00ce8c5f
Firas Al-Houssari <f.alhusari@gmail.com>
2021-02-28 00:44:02 +0200

    Added fields for registration

---
+++ EntityFrameworkAuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EntityFrameworkAuth.Data;
+using EntityFrameworkAuth.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EntityFrameworkAuth.Areas.Identity.IdentityHostingStartup))]
+namespace EntityFrameworkAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
